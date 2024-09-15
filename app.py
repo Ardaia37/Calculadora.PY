16cf66093440a78e848f060e96816d75391b92d6
Ardaia. <163694965+Ardaia37@users.noreply.github.com>
2024-09-15 09:31:04 -0400

    update app py

--- app.py
+++ app.py
@@ -16,7 +16,8 @@ janela.geometry("235x310")
 janela.config (bg=cor1)
 
 # criando parte calculavel
-Frame_tela =Frame(janela, width=235, height=50, bg=cor3)
+Frame_tela =Frame(janela, 
+                  width=235, height=50, bg=cor3)
 Frame_tela.grid(row=0, column=0)
 
 Frame_corpo =Frame(janela, width=235, height=268)
